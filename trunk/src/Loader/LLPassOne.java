1a5fa321efaacf7f54c0d069d0f8c53be6680a46
ben.trivett <ben.trivett@77b6f217-619d-a641-5199-e162ce2dd6c1>
2011-03-11 03:52:10 +0000

    I think I'm done? herp.

--- trunk/src/Loader/LLPassOne.java
+++ trunk/src/Loader/LLPassOne.java
@@ -6,140 +6,106 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import Assembler.Utility;
+
 /**
  * 
  * @author BSchuck
- *
+ * 
  */
 public class LLPassOne {
-/**
- * 
- * @param names
- * @return
- * @throws IOException
- */
+	/**
+	 * 
+	 * @param names
+	 * @return
+	 * @throws IOException
+	 */
 	public static String passOne(String[] names) throws IOException {
-		int PLA = 0;
+		int PLA = Utility.HexToDecimalValue(WilevenLoader.IPLA);
 		int total = WilevenLoader.totalSegLength;
-		int newPLA = 0;
-		String firstHeader = "";
 		String read = "";
-	
 
-		// read the first file
-		FileReader reader = new FileReader(names[0]);
-		BufferedReader file = new BufferedReader(reader);
-
-		// read the first line
-		read = file.readLine();
-
-		// to get the load location
-		firstHeader = overSubstring(read, 7, 10);
-		// check to see if it doesnt equal 0000 this means its absolute
-		if (!firstHeader.contentEquals("0000")) {
-			if (names.length > 1) {
-				return "There can not be mutiple files if one is absolute.";
-			}
-			// put the value of the start to the IPLA
-			WilevenLoader.IPLA = firstHeader;
-		} else {
-			// get the user input for the start address
-			System.out.print("Please enter a four digit "
-					+ "load location in HEX and hit enter: ");
+		// get the user input for the start address
+		System.out.print("Please enter a four digit "
+				+ "load location in HEX and hit enter: ");
+		Boolean isNotCorrectInput = true;
+		while (isNotCorrectInput) {
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					System.in));
-				WilevenLoader.IPLA = br.readLine();
-	
+			String readIPLA = br.readLine();
 
-			// check to make sure they enter a address that lets it all fit on
-			// one page
-			while ((Utility.HexToDecimalValue(WilevenLoader.IPLA) + total) > 65535) {
+			// Make sure they entered a hex address
+			if (!Utility.isHexString(readIPLA)) {
+				System.out.print("Please enter a valid 4 digit HEX number: ");
+
+				// check to make sure they enter a address that lets it all fit
+				// on one page
+			} else if ((Utility.HexToDecimalValue(readIPLA) + total) > 65535) {
 				System.out
-						.print("Please enter another four digit, that is smaller "
-								+ "load location in HEX and hit enter: ");
-				BufferedReader br1 = new BufferedReader(new InputStreamReader(
-						System.in));
-					WilevenLoader.IPLA = br1.readLine();
+						.print("Please enter a smaller HEX number and hit enter: ");
+			} else {
+				WilevenLoader.IPLA = readIPLA;
+				isNotCorrectInput = false;
+			}
+		}
+		
+		// iterate through all files and add data to external symbol table
+		int count = 0;
+		while (count < names.length) {
+
+			FileReader reader = new FileReader(names[count]);
+			BufferedReader file = new BufferedReader(reader);
 			
-
-			}
-		}
-
-		// read the next line
-		read = file.readLine();
-		while (read.charAt(0) == 'S') {
-			// set the location
-			PLA  = Utility.HexToDecimalValue(WilevenLoader.IPLA) + newPLA;
-			// add the length of the segment to the loaction
-			newPLA += WilevenLoader.place[0];
-			// get the index of the equals sign
-			int index = read.indexOf('=');
-			// get the name of the symbol
-			String name = overSubstring(read, 1, index);
-			// get the value of the symbol
-			String loc = overSubstring(read, index, index + 4);
-			// update the pla with the new value
-			String updatedPLA = Utility.DecimalValueToHex(PLA
-					+ Utility.HexToDecimalValue(loc));
-			// put them into the external symbol table
-			WilevenLoader.machineTables.externalSymbolTable.put(name, updatedPLA);
-			// check to see if it is already defined
-			if (WilevenLoader.machineTables.externalSymbolTable.containsKey(name)) {
-				return "The .ENT " + name + " symbol has already been defined";
-			}
-			// read the next line
-			read = file.readLine();
-		}
-
-		int count = 1;
-		// if there are more than one file
-		while (names.length > count) {
-
-			reader = new FileReader(names[count]);
-			file = new BufferedReader(reader);
-
 			// read the first line header record
 			read = file.readLine();
 			// read the next line the first text record
 			read = file.readLine();
-			while (read.charAt(0) == 'S') {
-				// set the location
-				PLA = Utility.HexToDecimalValue(WilevenLoader.IPLA) + newPLA;
-				// add the length of the segment to the loaction
-				newPLA += WilevenLoader.place[count];
-				// get the index of the equals sign
-				int index = read.indexOf('=');
-				// get the name of the symbol
-				String name = overSubstring(read, 1, index);
-				// get the value of the symbol
-				String loc = overSubstring(read, index, index + 4);
-				// update the pla with the new value
-				String updatedPLA = Utility.DecimalValueToHex(PLA
-						+ Utility.HexToDecimalValue(loc));
-				// put them into the external symbol table
-				WilevenLoader.machineTables.externalSymbolTable.put(name, updatedPLA);
-				// check to see if it is already defined
-				if (WilevenLoader.machineTables.externalSymbolTable.containsKey(name)) {
-					return "The .ENT " + name
-							+ " symbol has already been defined";
+
+			while (read.charAt(0) != 'E') {
+				if (read.charAt(0) == 'S') {
+					// get the index of the equals sign
+					int index = read.indexOf('=');
+					// get the name of the symbol
+					String name = overSubstring(read, 1, index);
+					// get the value of the symbol
+					String loc = overSubstring(read, index + 1, index + 5);
+					// update the pla with the new value
+					String updatedPLA = Utility.DecimalValueToHex(PLA
+							+ Utility.HexToDecimalValue(loc));
+					// check to see if it is already defined
+					if (WilevenLoader.machineTables.externalSymbolTable
+							.containsKey(name)) {
+						return "The .ENT symbol " + name
+								+ " has already been defined";
+					}
+					// put them into the external symbol table
+					WilevenLoader.machineTables.externalSymbolTable.put(name,
+							updatedPLA);
 				}
 				// read the next line
 				read = file.readLine();
 			}
-			//get the next file
+			
+			// Close readers for current file.
+			reader.close();
+			file.close();
+			
+			// add length of previous segment
+			PLA += WilevenLoader.place[count];
+			
+			// get the next file
 			count++;
 		}
 
 		return null;
-
 	}
-/**
- * 
- * @param str
- * @param x
- * @param y
- * @return
- */
+
+	/**
+	 * 
+	 * @param str
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	private static String overSubstring(String str, int x, int y) {
 		Boolean exceptions = true;
 		int z = 0;
---
+++ trunk/src/Loader/LLPassTwo.java
@@ -0,0 +1,218 @@
+package Loader;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import Assembler.Utility;
+
+/**
+ * 
+ * @author Ben Trivett
+ * 
+ */
+
+public class LLPassTwo {
+
+	/**
+	 * Builds an absolute object file (with the name of the first file in the
+	 * array + FINAL) from all the object files inputed.
+	 * 
+	 * @param names
+	 * @return any errors that occur or null for successful execution.
+	 * @throws IOException
+	 */
+	public static String output(String[] names) throws IOException {
+		// Receive the String array from the assemble with all the file names
+		// Remove the first name
+		String firstFile = names[0];
+		String read = "";
+		String end = "";
+
+		int PLA = Utility.HexToDecimalValue(WilevenLoader.IPLA);
+		int total = WilevenLoader.totalSegLength;
+
+		// read from the first file inputed
+		FileReader reader = new FileReader(firstFile);
+		BufferedReader file = new BufferedReader(reader);
+
+		// append FINAL to end of first file's name for the output file.
+		int firstFileIndex = firstFile.indexOf(".o");
+		firstFile = firstFile.substring(0, firstFileIndex) + "FINAL.o";
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(
+				firstFile));
+
+		// read in the first line
+		read = file.readLine();
+		String header = "";
+
+		// change the letter to H to match simulator object file format
+		if (read.charAt(0) == 'G') {
+			header = 'H'
+					+ read.substring(1, 7)
+					+ Utility.DecimalValueToHex(PLA
+							+ Utility.HexToDecimalValue(read.substring(7, 11)))
+					+ Utility.DecimalValueToHex(total);
+		} else {
+			header = read;
+		}
+
+		// write it to the new file
+		bufferedWriter.write(header);
+		bufferedWriter.newLine();
+
+		// read the next line
+		read = file.readLine();
+		while (read.charAt(0) != 'E') {
+			// Update the load address with the PLA
+			read = read.substring(0, 1)
+					+ Utility.DecimalValueToHex(Utility.HexToDecimalValue(read
+							.substring(1, 5)) + PLA) + read.substring(5);
+
+			// check to see if it is a P and change it to a T and update the
+			// last 9 bits with the PLA
+			if (read.charAt(0) == 'P') {
+				read = 'T'
+						+ read.substring(1, 5)
+						+ Utility
+								.BinaryToHex(Utility.HexToBinary(
+										read.substring(5)).substring(0, 7)
+										+ Utility
+												.HexToBinary(
+														Utility.DecimalValueToHex((Utility
+																.HexToDecimalValue(Utility
+																		.BinaryToHex(Utility
+																				.HexToBinary(
+																						read.substring(5))
+																				.substring(
+																						7))) + PLA)))
+												.substring(7));
+				// write it to the new file
+				bufferedWriter.write(read);
+				bufferedWriter.newLine();
+			} else if (read.charAt(0) == 'S') {
+				// Ignored, they are no longer needed.
+			} else if (read.charAt(0) == 'X') {
+				// Update the last 9 bits with the value of the external symbol
+				String extSymbol = read.substring(9);
+				if (WilevenLoader.machineTables.externalSymbolTable
+						.containsKey(extSymbol)) {
+					extSymbol = WilevenLoader.machineTables.externalSymbolTable
+							.get(extSymbol);
+					read = read.substring(0, 9);
+				} else {
+					return "The external symbol " + extSymbol
+							+ " was not defined in " + names[0] + ".";
+				}
+				read = 'T'
+						+ read.substring(1, 5)
+						+ Utility.BinaryToHex(Utility.HexToBinary(
+								read.substring(5)).substring(0, 7)
+								+ Utility.HexToBinary(extSymbol).substring(7));
+				// write it to the new file
+				bufferedWriter.write(read);
+				bufferedWriter.newLine();
+			}
+
+			// read in the next line
+			read = file.readLine();
+		}
+
+		// Build end record by adding IPLA to it's value.
+		end = "E"
+				+ Utility.DecimalValueToHex(Utility.HexToDecimalValue(read
+						.substring(1)) + PLA);
+
+		int count = 1;
+		// while there are more than 1 file keep reading them
+		while (count < names.length) {
+
+			// Update PLA with the segment length of the last file.
+			PLA += WilevenLoader.place[count - 1];
+
+			// read from the next file
+			reader = new FileReader(names[count]);
+			file = new BufferedReader(reader);
+
+			// read the header
+			read = file.readLine();
+			// read in the first record
+			read = file.readLine();
+
+			while (read.charAt(0) != 'E') {
+				// Update the load address with the PLA
+				read = read.substring(0, 1)
+						+ Utility.DecimalValueToHex(Utility
+								.HexToDecimalValue(read.substring(1, 5)) + PLA)
+						+ read.substring(5);
+
+				// check to see if it is a P and change it to a T and update the
+				// last 9 bits with the PLA
+				if (read.charAt(0) == 'P') {
+					read = 'T'
+							+ read.substring(1, 5)
+							+ Utility
+									.BinaryToHex(Utility.HexToBinary(
+											read.substring(5)).substring(0, 7)
+											+ Utility
+													.HexToBinary(
+															Utility.DecimalValueToHex((Utility
+																	.HexToDecimalValue(Utility
+																			.BinaryToHex(Utility
+																					.HexToBinary(
+																							read.substring(5))
+																					.substring(
+																							7))) + PLA)))
+													.substring(7));
+					// write it to the new file
+					bufferedWriter.write(read);
+					bufferedWriter.newLine();
+
+				} else if (read.charAt(0) == 'T') {
+					// write it to the new file
+					bufferedWriter.write(read);
+					bufferedWriter.newLine();
+
+				} else if (read.charAt(0) == 'X') {
+					// Update the last 9 bits with the value of the external
+					// symbol
+					String extSymbol = read.substring(10);
+					if (WilevenLoader.machineTables.externalSymbolTable
+							.containsKey(extSymbol)) {
+						extSymbol = WilevenLoader.machineTables.externalSymbolTable
+								.get(extSymbol);
+						read = read.substring(0, 10);
+					} else {
+						return "The external symbol " + extSymbol
+								+ " was not defined in " + names[count] + ".";
+					}
+					read = 'T'
+							+ read.substring(1, 5)
+							+ Utility.BinaryToHex(Utility.HexToBinary(
+									read.substring(5)).substring(0, 7)
+									+ Utility.HexToBinary(extSymbol).substring(
+											7));
+					// write it to the new file
+					bufferedWriter.write(read);
+					bufferedWriter.newLine();
+				}
+
+				// read in the next line
+				read = file.readLine();
+			}
+			count++;
+
+		}
+
+		// write the end record
+		bufferedWriter.write(end);
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+		reader.close();
+		file.close();
+
+		return null;
+	}
+}
