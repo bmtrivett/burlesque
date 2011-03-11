e7043a1744caf4816fcb91864a4bb506c54472ec
ben.trivett <ben.trivett@77b6f217-619d-a641-5199-e162ce2dd6c1>
2011-03-11 05:05:34 +0000

    Fixed some stupid errors that I did. Now I'm getting an external symbol added to the symbol table when there is none?

--- src/Loader/LLPassTwo.java
+++ src/Loader/LLPassTwo.java
@@ -66,82 +66,9 @@ public class LLPassTwo {
 		// read the next line
 		read = file.readLine();
 		while (read.charAt(0) != 'E') {
-			// Update the load address with the PLA
-			read = read.substring(0, 1)
-					+ Utility.DecimalValueToHex(Utility.HexToDecimalValue(read
-							.substring(1, 5)) + PLA) + read.substring(5);
-
-			// check to see if it is a P and change it to a T and update the
-			// last 9 bits with the PLA
-			if (read.charAt(0) == 'P') {
-				read = 'T'
-						+ read.substring(1, 5)
-						+ Utility
-								.BinaryToHex(Utility.HexToBinary(
-										read.substring(5)).substring(0, 7)
-										+ Utility
-												.HexToBinary(
-														Utility.DecimalValueToHex((Utility
-																.HexToDecimalValue(Utility
-																		.BinaryToHex(Utility
-																				.HexToBinary(
-																						read.substring(5))
-																				.substring(
-																						7))) + PLA)))
-												.substring(7));
-				// write it to the new file
-				bufferedWriter.write(read);
-				bufferedWriter.newLine();
-			} else if (read.charAt(0) == 'S') {
-				// Ignored, they are no longer needed.
-			} else if (read.charAt(0) == 'X') {
-				// Update the last 9 bits with the value of the external symbol
-				String extSymbol = read.substring(9);
-				if (WilevenLoader.machineTables.externalSymbolTable
-						.containsKey(extSymbol)) {
-					extSymbol = WilevenLoader.machineTables.externalSymbolTable
-							.get(extSymbol);
-					read = read.substring(0, 9);
-				} else {
-					return "The external symbol " + extSymbol
-							+ " was not defined in " + names[0] + ".";
-				}
-				read = 'T'
-						+ read.substring(1, 5)
-						+ Utility.BinaryToHex(Utility.HexToBinary(
-								read.substring(5)).substring(0, 7)
-								+ Utility.HexToBinary(extSymbol).substring(7));
-				// write it to the new file
-				bufferedWriter.write(read);
-				bufferedWriter.newLine();
-			}
-
-			// read in the next line
-			read = file.readLine();
-		}
-
-		// Build end record by adding IPLA to it's value.
-		end = "E"
-				+ Utility.DecimalValueToHex(Utility.HexToDecimalValue(read
-						.substring(1)) + PLA);
-
-		int count = 1;
-		// while there are more than 1 file keep reading them
-		while (count < names.length) {
-
-			// Update PLA with the segment length of the last file.
-			PLA += WilevenLoader.place[count - 1];
-
-			// read from the next file
-			reader = new FileReader(names[count]);
-			file = new BufferedReader(reader);
-
-			// read the header
-			read = file.readLine();
-			// read in the first record
-			read = file.readLine();
-
-			while (read.charAt(0) != 'E') {
+			if (read.charAt(0) == 'S') {
+				// Do nothing. S lines are ignored.
+			} else {
 				// Update the load address with the PLA
 				read = read.substring(0, 1)
 						+ Utility.DecimalValueToHex(Utility
@@ -169,12 +96,8 @@ public class LLPassTwo {
 					// write it to the new file
 					bufferedWriter.write(read);
 					bufferedWriter.newLine();
-
-				} else if (read.charAt(0) == 'T') {
-					// write it to the new file
-					bufferedWriter.write(read);
-					bufferedWriter.newLine();
-
+				} else if (read.charAt(0) == 'S') {
+					// Ignored, they are no longer needed.
 				} else if (read.charAt(0) == 'X') {
 					// Update the last 9 bits with the value of the external
 					// symbol
@@ -186,7 +109,7 @@ public class LLPassTwo {
 						read = read.substring(0, 9);
 					} else {
 						return "The external symbol " + extSymbol
-								+ " was not defined in " + names[count] + ".";
+								+ " was not defined in " + names[0] + ".";
 					}
 					read = 'T'
 							+ read.substring(1, 5)
@@ -198,7 +121,96 @@ public class LLPassTwo {
 					bufferedWriter.write(read);
 					bufferedWriter.newLine();
 				}
+			}
+			// read in the next line
+			read = file.readLine();
+		}
 
+		// Build end record by adding IPLA to it's value.
+		end = "E"
+				+ Utility.DecimalValueToHex(Utility.HexToDecimalValue(read
+						.substring(1)) + PLA);
+
+		int count = 1;
+		// while there are more than 1 file keep reading them
+		while (count < names.length) {
+
+			// Update PLA with the segment length of the last file.
+			PLA += WilevenLoader.place[count - 1];
+
+			// read from the next file
+			reader = new FileReader(names[count]);
+			file = new BufferedReader(reader);
+
+			// read the header
+			read = file.readLine();
+			// read in the first record
+			read = file.readLine();
+
+			while (read.charAt(0) != 'E') {
+				if (read.charAt(0) == 'S') {
+					// Do nothing. S lines are ignored.
+				} else {
+					// Update the load address with the PLA
+					read = read.substring(0, 1)
+							+ Utility.DecimalValueToHex(Utility
+									.HexToDecimalValue(read.substring(1, 5))
+									+ PLA) + read.substring(5);
+
+					// check to see if it is a P and change it to a T and update
+					// the
+					// last 9 bits with the PLA
+					if (read.charAt(0) == 'P') {
+						read = 'T'
+								+ read.substring(1, 5)
+								+ Utility
+										.BinaryToHex(Utility.HexToBinary(
+												read.substring(5)).substring(0,
+												7)
+												+ Utility
+														.HexToBinary(
+																Utility.DecimalValueToHex((Utility
+																		.HexToDecimalValue(Utility
+																				.BinaryToHex(Utility
+																						.HexToBinary(
+																								read.substring(5))
+																						.substring(
+																								7))) + PLA)))
+														.substring(7));
+						// write it to the new file
+						bufferedWriter.write(read);
+						bufferedWriter.newLine();
+
+					} else if (read.charAt(0) == 'T') {
+						// write it to the new file
+						bufferedWriter.write(read);
+						bufferedWriter.newLine();
+
+					} else if (read.charAt(0) == 'X') {
+						// Update the last 9 bits with the value of the external
+						// symbol
+						String extSymbol = read.substring(9);
+						if (WilevenLoader.machineTables.externalSymbolTable
+								.containsKey(extSymbol)) {
+							extSymbol = WilevenLoader.machineTables.externalSymbolTable
+									.get(extSymbol);
+							read = read.substring(0, 9);
+						} else {
+							return "The external symbol " + extSymbol
+									+ " was not defined in " + names[count]
+									+ ".";
+						}
+						read = 'T'
+								+ read.substring(1, 5)
+								+ Utility.BinaryToHex(Utility.HexToBinary(
+										read.substring(5)).substring(0, 7)
+										+ Utility.HexToBinary(extSymbol)
+												.substring(7));
+						// write it to the new file
+						bufferedWriter.write(read);
+						bufferedWriter.newLine();
+					}
+				}
 				// read in the next line
 				read = file.readLine();
 			}
